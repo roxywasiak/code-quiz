e485583140df5bcfab9d9628d637de37d405dbf6
roxywasiak <ruksclone@hotmail.com>
2022-04-23 18:42:24 +0100

    styled my page and added my colors with specific sizing and position

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+//gloabal declarations
+const questions = [];
+let questionIndex = 0;
+let timerValue = 10 * questions.length;
+let quizComplete;
+
+//when the page loads to the browser
+const onLoad = () => {};
+
+const removeStartSection = () => {};
+
+const startTimer = () => {
+  //another function to execute every second
+  const countdown = () => {
+    //decrease the time
+    // if quizComplete is true then stop the timer
+    // check if timer reaches 0
+    //if true then game is over
+  };
+  //set interval of 1000ms (1s)
+};
